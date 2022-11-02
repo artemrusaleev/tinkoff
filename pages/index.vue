a356eecc1de3483baef4721747857282205e1e8b
Artem Rusaleev <artyomrusaleev@gmail.com>
2022-11-02 17:10:46 +0300

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,101 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="main-page">
+    <v-form ref="form" v-model="valid">
+      <v-text-field
+        v-model="token"
+        :rules="tokenRules"
+        label="Token"
+        required
+      ></v-text-field>
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="submit">
+        Submit
+      </v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  data: () => ({
+    valid: true,
+    token: '',
+    tokenRules: [(v) => !!v || 'Login is required'],
+  }),
+
+  methods: {
+    async submit() {
+      if (this.$refs.form.validate()) {
+        const myHeaders = new Headers()
+        myHeaders.append(
+          'Authorization',
+          'Bearer 9c8e141c701a488f1c86cb9c782e17a0'
+        )
+        myHeaders.append(
+          'Cookie',
+          'JSESSIONID=97F3D89C7B9CE220D138B413694E3756'
+        )
+        const body = {
+          billingDescriptor: 'narodsushi',
+          fullName: 'ИП ЛАЕР ПАВЕЛ ПАВЛОВИЧ',
+          name: 'ИП ЛАЕРПАВЕЛ ПАВЛОВИЧ',
+          inn: 190701090299,
+          kpp: '-',
+          ogrn: 322190000015390,
+          addresses: [
+            {
+              type: 'legal',
+              zip: '655350',
+              country: 'RUS',
+              city: 'с. Знаменка',
+              street: 'УЛ НАГОРНАЯ, дом 24',
+            },
+          ],
+          email: 'laer_1981@mail.ru',
+          founders: {
+            individuals: [
+              {
+                firstName: 'ПАВЕЛ',
+                lastName: 'ЛАЕР',
+                citizenship: 'С ЗНАМЕНКА',
+                address: 'УЛ НАГОРНАЯ, дом 24',
+              },
+            ],
+          },
+          ceo: {
+            address: 'УЛ НАГОРНАЯ, дом 24',
+            firstName: 'ПАВЕЛ',
+            lastName: 'ЛАЕР',
+            middleName: '',
+            phone: '79833728965',
+            country: 'RUS',
+          },
+          siteUrl: 'https://narodsushi.ru',
+          bankAccount: {
+            account: '40802810700003555415',
+            korAccount: '30101810145250000974',
+            bankName: 'АО «Тинькофф Банк»',
+            bik: 44525974,
+            details: '',
+            tax: 1,
+          },
+          fiscalization: {
+            company: 'АТОЛ онлайн',
+            notifyUrl: '',
+          },
+        }
+        const fetchOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: JSON.stringify(body),
+        }
+        const req = await fetch(
+          'https://sm-register.tinkoff.ru/register',
+          fetchOptions
+        )
+        const response = await req.text()
+        console.log(response)
+      }
+    },
+  },
 }
 </script>
